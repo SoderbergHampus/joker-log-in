8a7b8236264d49fcda1ffe8a6839d617333f400c
SoderbergHampus <soderberg.dev@gmail.com>
2024-03-08 15:18:15 +0100

    Trying a changed base in vite

--- jokes-demo-client/vite.config.ts
+++ jokes-demo-client/vite.config.ts
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(() => ({
   plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
   },
-  base: './'
-});
+  base: './joker-log-in',
+}));
